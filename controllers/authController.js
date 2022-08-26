443c2ead46553288d03936f338296e6a31344353
efekanbicermac <efekan.bicer8@gmail.com>
2022-08-27 00:30:01 +0300

    enroll and release course

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,11 @@
 const User = require("../models/User");
+const Category = require("../models/Category")
+const Course = require('../models/Course')
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose');
 const { use } = require("../routes/userRoute");
+
+
 
 
 
@@ -9,10 +13,7 @@ exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
 
-    res.status(201).json({
-      status: "success",
-      user,
-    });
+  res.status(201).redirect('/login');
   } catch (error) {
     res.status(400).json({
       status: "fail",
@@ -31,7 +32,7 @@ exports.loginUser = async (req, res) => {
          bcrypt.compare(password, user.password, (err,same) => {
           if(same){
             req.session.userID = user._id
-            res.status(200).redirect('/')
+            res.status(200).redirect('/users/dashboard');
           }
          })
       }
@@ -49,5 +50,19 @@ exports.logoutUser = (req, res) => {
     res.redirect('/');
   })
 }
+
+exports.getDashboardPage = async (req, res) => {
+   const user = await User.findOne({_id:req.session.userID}).populate('courses');
+  const categories = await Category.find()
+  const courses = await Course.find({user:req.session.userID})
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
+    categories,
+    courses
+  });
+}; 
+  
+
 
 
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,14 +1,15 @@
-const pageController = require('../controllers/pageController')
 const express = require('express')
-
+const pageController = require('../controllers/pageController')
+const redirectMiddleware = require('../middlewares/redirectMiddleware')
 
 const router = express.Router()
 
 router.route('/').get(pageController.getIndexPage)
 router.route('/about').get(pageController.getAboutPage)
-router.route('/register').get(pageController.getRegisterPage)
-router.route('/login').get(pageController.getLoginPage)
+router.route('/register').get(redirectMiddleware, pageController.getRegisterPage);
+router.route('/login').get(redirectMiddleware, pageController.getLoginPage);
 router.route('/contact').get(pageController.getContactPage)
+router.route('/contact').post(pageController.sendEmail)
 router.route('/pricing').get(pageController.getPricingPage)
 router.route('/blog').get(pageController.getBlogPage)
 
